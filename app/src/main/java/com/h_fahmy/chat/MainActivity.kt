85398a7d5b154bdfe4d3906668570b2bc2829752
Hussien <hussien.fahmy98@gmail.com>
2023-10-07 20:56:59 +0300

    Added the two screens to MainActivity

--- app/src/main/java/com/h_fahmy/chat/MainActivity.kt
+++ app/src/main/java/com/h_fahmy/chat/MainActivity.kt
@@ -3,23 +3,43 @@ package com.h_fahmy.chat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.h_fahmy.chat.presentation.chat.ChatScreen
+import com.h_fahmy.chat.presentation.username.UsernameScreen
 import com.h_fahmy.chat.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "username_screen"
                 ) {
+                    composable("username_screen") {
+                        UsernameScreen(onNavigate = navController::navigate)
+                    }
 
+                    composable(
+                        route = "chat_screen/{username}",
+                        arguments = listOf(
+                            navArgument(name = "username") {
+                                type = NavType.StringType
+                                nullable = true
+                            }
+                        )
+                    ) { backStackEntry ->
+                        val username = backStackEntry.arguments?.getString("username")
+                        ChatScreen(username = username,)
+                    }
                 }
             }
         }
